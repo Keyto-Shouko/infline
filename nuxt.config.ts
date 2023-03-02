4c9c88c65b6661320429958d7e0210a3edefde81
Keyto-Shouko <balvesca@gmail.com>
2023-03-02 11:20:30 +0100

    added font, changed config, home page wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
-    },
-  })
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    config: {
+      plugins: [
+        require('@tailwindcss/forms')
+      ],
+      content: []
+    }
+  }
+}) 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
-  ],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        integral: ["Integral CF", "sans-serif"],
+      }
+    },
   },
-  plugins: [],
 }
