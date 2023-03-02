a69a571e4492737a9326d865b7d8e15fa7998619
Keyto-Shouko <balvesca@gmail.com>
2023-03-02 11:15:02 +0100

    fresh tailwind import, doing home page

--- pages/home.vue
+++ pages/home.vue
@@ -3,14 +3,24 @@
 </script>
 
 <template>
-  <div class="flex justify-center bg-black text-white w-screen">
-    <h1 class="" id="inflineTitle">INFLINE</h1>
-  </div>
+    <div class="bg-black">
+        <div class="flex justify-center text-white flex-wrap">
+            <h1 class="font-integral">INFLINE</h1>
+        </div>
+        <div class="mt-8">
+            <h1 class="font-integral bg-black text-white text-2xl">
+                LA PLATEFORME N°1 POUR TROUVER UN
+            </h1>
+            <span class="outlined text-black text-2xl">INFLUENCEUR</span>
+            <div class="">
+                <img src="~/assets/images/selfieGirl.png" alt="selfieGirlPlaceHolder" srcset="">
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-  #inflineTitle{
-    font-family: 'IntegralCF';
-    font-weight: 400;
-  }
+.outlined {
+    text-shadow: -1px -1px 0 #b188ea, 1px -1px 0 #b188ea, -1px 1px 0 #b188ea, 1px 1px 0 #b188ea;
+}
 </style>
