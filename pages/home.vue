3ee646c336dc37ec1607124ec4691061049708d7
Keyto-Shouko <balvesca@gmail.com>
2023-03-02 13:53:54 +0100

    finished the home page (i guess)

--- pages/home.vue
+++ pages/home.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
-
+defineProps<{
+    buttonText1: string,
+    buttonText2: string,
+}>()
 </script>
 
 <template>
     <div class="bg-black">
-        <div class="flex justify-center text-white flex-wrap">
+        <div class="flex justify-center text-white flex-wrap p-4">
             <h1 class="font-integral">INFLINE</h1>
         </div>
-        <div class="mt-8">
-            <h1 class="font-integral bg-black text-white text-2xl">
-                LA PLATEFORME N°1 POUR TROUVER UN
-            </h1>
-            <span class="outlined text-black text-2xl">INFLUENCEUR</span>
-            <div class="">
+        <div class="mt-24 ml-28 flex justify-between max-w-[75%]">
+            <div class="w-[45%]">
+                <p class="font-integral bg-black text-white text-5xl mt-12 w-[98%]">
+                    LA PLATEFORME N°1 POUR TROUVER UN
+                    <span class="outlined text-black text-5xl w-1">INFLUENCEUR</span>
+                </p>
+                
+                <button class="font-integral text-black rounded-lg bg-white p-4 mt-11 mr-11">
+                    JE SUIS INFLUENCEUR
+                </button>
+                <button class="font-integral text-white rounded-lg bg-purpleMDS p-4">
+                    JE SUIS UNE MARQUE
+                </button>
+            </div>
+            <div class="h-1/3 w-1/3 bg-contain">
                 <img src="~/assets/images/selfieGirl.png" alt="selfieGirlPlaceHolder" srcset="">
             </div>
         </div>
+
     </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   theme: {
     extend: {
-
+      colors: {
+        'purpleMDS':'#B188EA',
+      },
       fontFamily: {
         integral: ["Integral CF", "sans-serif"],
       }
